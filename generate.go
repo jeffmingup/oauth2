4fe97d7050a73fe1cf769b6ac9f3e4cba404ef32
wangmingming <1960588251@qq.com>
2021-09-10 11:15:18 +0800

    update

--- generate.go
+++ generate.go
@@ -7,21 +7,20 @@ import (
 )
 
 // GenerateBasic provide the basis of the generated token data
-	type	GenerateBasic struct {
-		Client    ClientInfo
-		UserID    string
-		CreateAt  time.Time
-		TokenInfo TokenInfo
-		Request   *http.Request
-	}
+type GenerateBasic struct {
+	Client    ClientInfo
+	UserID    string
+	CreateAt  time.Time
+	TokenInfo TokenInfo
+	Request   *http.Request
+}
 
-	// AuthorizeGenerate generate the authorization code interface
-	type AuthorizeGenerate interface {
-		Token(ctx context.Context, data *GenerateBasic) (code string, err error)
-	}
+// AuthorizeGenerate generate the authorization code interface
+type AuthorizeGenerate interface {
+	Token(ctx context.Context, data *GenerateBasic) (code string, err error)
+}
 
-	// AccessGenerate generate the access and refresh tokens interface
-	type AccessGenerate interface {
-		Token(ctx context.Context, data *GenerateBasic, isGenRefresh bool) (access, refresh string, err error)
-	}
-
+// AccessGenerate generate the access and refresh tokens interface
+type AccessGenerate interface {
+	Token(ctx context.Context, data *GenerateBasic, isGenRefresh bool) (access, refresh string, err error)
+}
--- model.go
+++ model.go
@@ -4,56 +4,54 @@ import (
 	"time"
 )
 
-type (
-	// ClientInfo the client information model interface
-	ClientInfo interface {
-		GetID() string
-		GetSecret() string
-		GetDomain() string
-		GetUserID() string
-	}
+// ClientInfo the client information model interface
+type ClientInfo interface {
+	GetID() string
+	GetSecret() string
+	GetDomain() string
+	GetUserID() string
+}
 
-	// ClientPasswordVerifier the password handler interface
-	ClientPasswordVerifier interface {
-		VerifyPassword(string) bool
-	}
+// ClientPasswordVerifier the password handler interface
+type ClientPasswordVerifier interface {
+	VerifyPassword(string) bool
+}
 
-	// TokenInfo the token information model interface
-	TokenInfo interface {
-		New() TokenInfo
+// TokenInfo the token information model interface
+type TokenInfo interface {
+	New() TokenInfo
 
-		GetClientID() string
-		SetClientID(string)
-		GetUserID() string
-		SetUserID(string)
-		GetRedirectURI() string
-		SetRedirectURI(string)
-		GetScope() string
-		SetScope(string)
+	GetClientID() string
+	SetClientID(string)
+	GetUserID() string
+	SetUserID(string)
+	GetRedirectURI() string
+	SetRedirectURI(string)
+	GetScope() string
+	SetScope(string)
 
-		GetCode() string
-		SetCode(string)
-		GetCodeCreateAt() time.Time
-		SetCodeCreateAt(time.Time)
-		GetCodeExpiresIn() time.Duration
-		SetCodeExpiresIn(time.Duration)
-		GetCodeChallenge() string
-		SetCodeChallenge(string)
-		GetCodeChallengeMethod() CodeChallengeMethod
-		SetCodeChallengeMethod(CodeChallengeMethod)
+	GetCode() string
+	SetCode(string)
+	GetCodeCreateAt() time.Time
+	SetCodeCreateAt(time.Time)
+	GetCodeExpiresIn() time.Duration
+	SetCodeExpiresIn(time.Duration)
+	GetCodeChallenge() string
+	SetCodeChallenge(string)
+	GetCodeChallengeMethod() CodeChallengeMethod
+	SetCodeChallengeMethod(CodeChallengeMethod)
 
-		GetAccess() string
-		SetAccess(string)
-		GetAccessCreateAt() time.Time
-		SetAccessCreateAt(time.Time)
-		GetAccessExpiresIn() time.Duration
-		SetAccessExpiresIn(time.Duration)
+	GetAccess() string
+	SetAccess(string)
+	GetAccessCreateAt() time.Time
+	SetAccessCreateAt(time.Time)
+	GetAccessExpiresIn() time.Duration
+	SetAccessExpiresIn(time.Duration)
 
-		GetRefresh() string
-		SetRefresh(string)
-		GetRefreshCreateAt() time.Time
-		SetRefreshCreateAt(time.Time)
-		GetRefreshExpiresIn() time.Duration
-		SetRefreshExpiresIn(time.Duration)
-	}
-)
+	GetRefresh() string
+	SetRefresh(string)
+	GetRefreshCreateAt() time.Time
+	SetRefreshCreateAt(time.Time)
+	GetRefreshExpiresIn() time.Duration
+	SetRefreshExpiresIn(time.Duration)
+}
